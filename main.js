5b7dd1685f03fc5b0de90f37a7371db33bd664a9
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-03-08 22:59:05 -0700

    Add nav active class toggle for nav

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
+const nav = document.querySelector('.navigation')
 const navBtn = document.querySelector('.nav_toggle')
 navBtn.addEventListener("click",navToggle)
 
 function navToggle(){
   console.log('in here')
   navBtn.classList.toggle('active')
+  nav.classList.toggle('active')
 }
